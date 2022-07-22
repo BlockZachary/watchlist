0285d514b35f927bb905c87061a9aeaaccbec074
BlockZachary <zachary26626@gmail.com>
2022-07-22 11:10:26 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
 from flask import Flask, render_template
 from markupsafe import escape
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+import os,click
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-name = 'Zachary Block'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+
+# name = 'Zachary Block'
+# movies = [
+#     {'title': 'My Neighbor Totoro', 'year': '1988'},
+#     {'title': 'Dead Poets Society', 'year': '1989'},
+#     {'title': 'A Perfect World', 'year': '1993'},
+#     {'title': 'Leon', 'year': '1994'},
+#     {'title': 'Mahjong', 'year': '1996'},
+#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#     {'title': 'King of Comedy', 'year': '1999'},
+#     {'title': 'Devils on the Doorstep', 'year': '1999'},
+#     {'title': 'WALL-E', 'year': '2008'},
+#     {'title': 'The Pork of Music', 'year': '2012'},
+# ]
 
 
 @app.route('/')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html',user=user,movies=movies)
 
 
 @app.route('/user/<name>')
@@ -39,3 +59,42 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+        click.echo('Drop all.')
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Zachary Block'
+    movies = [
+        {'title': '请回答1988', 'year': '1988'},
+        {'title': '奇迹·笨小孩', 'year': '2022'},
+        {'title': '绿皮书 Green Book', 'year': '2018'},
+        {'title': '忠犬八公的故事 Hachi: A Dog\'s Tale', 'year': '2009'},
+        {'title': '扬名立万', 'year': '2021'},
+        {'title': '唐人街探案', 'year': '2015'},
+        {'title': '西虹市首富', 'year': '2018'},
+        {'title': '疯狂动物城 Zootopia', 'year': '2016'},
+        {'title': '情书 Love Letter', 'year': '1995'},
+        {'title': '花束般的恋爱 花束みたいな恋をした', 'year': '2021'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
